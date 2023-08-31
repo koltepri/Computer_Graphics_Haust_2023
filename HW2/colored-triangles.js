aced85ddc5ab78ef783dc6329ea8d138a7baa784
koltepri <odinnagust@protonmail.com>
2023-08-31 11:15:31 +0000

    Math.pi breytt i Math.PI

--- HW2/colored-triangles.js
+++ HW2/colored-triangles.js
@@ -11,16 +11,16 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
     var triangle_size = 0.3;
-    var triangle_angle = Math.pi/3;
+    var triangle_angle = Math.PI/3;
     points = []; // vandaddi semikommu...
 
     for(let i = 0; i < 100; i++) {
       p1X = Math.random() * -1.0;
       p1Y = Math.random() * -1.0;
-      p2X = p1X + Math.cos(4*Math.pi/3) * triangle_size;
-      p2Y = p1Y + Math.sin(4*Math.pi/3) * triangle_size;
-      p3X = p1X + Math.cos(5*Math.pi/3) * triangle_size;
-      p3Y = p1Y + Math.sin(5*Math.pi/3) * triangle_size;
+      p2X = p1X + Math.cos(4*Math.PI/3) * triangle_size;
+      p2Y = p1Y + Math.sin(4*Math.PI/3) * triangle_size;
+      p3X = p1X + Math.cos(5*Math.PI/3) * triangle_size;
+      p3Y = p1Y + Math.sin(5*Math.PI/3) * triangle_size;
       points.push(p1X,p1Y,p2X,p2Y,p3X,p3Y);
     }
     console.log(points);
@@ -52,13 +52,13 @@ window.onload = function init()
 
 
 function render() {
-    for(let i = 0; i < 9; i+=3) {
+    for(let i = 0; i < 9; i+=6) {
       var R = Math.random();
       var G = Math.random();
       var B = Math.random();
       //var color = vec4(1.0,0.5,0.5,1.0);
       gl.clear( gl.COLOR_BUFFER_BIT );
       gl.uniform4fv( colorLoc,vec4(1.0,0.5,0.5,1.0) ); // vec4 not defined
-      gl.drawArrays( gl.TRIANGLES, i, 3);
+      gl.drawArrays( gl.TRIANGLES, i, 6);
     }
 }
