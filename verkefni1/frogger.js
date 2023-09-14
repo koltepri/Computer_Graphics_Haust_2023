df2a2b839c371266b38a4acde5c368c2687a3f3b
koltepri <odinnagust@protonmail.com>
2023-09-14 11:17:02 +0000

    ný aðferð. gera mest alla vinnunna fyrir utan render fallið, er aðallega bara að vesenast með buffers og tengingu við shaderana

--- verkefni1/frogger.js
+++ verkefni1/frogger.js
@@ -15,6 +15,7 @@ var locColor;
 
 
 window.onload = function init() {
+    // --- BoilerPlate
     canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
@@ -26,16 +27,24 @@ window.onload = function init() {
     //  Load shaders and initialize attribute buffers
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+    // --- 
+    var verticesTiles = createBoxes();
 
-    verticesTiles = createBoxes();
-
-    bufferTiles = gl.createBuffer();
+    var bufferTiles = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,bufferTiles);
-    gl.bufferData(gl.ARRAY_BUFFER,flatten(verticesTiles),gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER,flatten(verticesTiles),gl.STATIC_DRAW);
 
-    locPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.enableVertexAttribArray( locPosition );
-    locColor = gl.getUniformLocation( program, "rcolor" );
+    var vPosition = gl.getAttribLocation(program,"vPosition");
+    gl.vertexAttribPointer(vPosition,2,gl.FLOAT,false,0,0);
+    gl.enableVertexAttribArray( vPosition );
+
+    var bufferTilesColor = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferTilesColor);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorTiles),gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
     
     render();
 
@@ -44,11 +53,7 @@ window.onload = function init() {
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-
     for(let i = 0; i < 3+nrOfLanes; i++) {
-      gl.bindBuffer( gl.ARRAY_BUFFER, bufferTiles);
-      gl.vertexAttribPointer( locPosition, 2, gl.FLOAT, false, 0, 0 );
-      gl.uniform4fv( locColor, flatten(colorTiles[i]) );
       gl.drawArrays( gl.TRIANGLE_FAN, i*4, 4);
     }
 }
@@ -67,7 +72,7 @@ function createBoxes() {
     let p3 = vec2(x0,-1+boxYSize*(i+1))
     vertices.push(p0,p1,p2,p3)
     if (i == 0 || i == totalSplits-1) { // upphafs og endapunktar
-      colorTiles.push(vec4(1.0,1.0,1.0,1.0));
+      colorTiles.push(vec4(1.0,1.0,0.0,1.0));
     }
     else if (i == totalSplits) { // stigasvaedi
       colorTiles.push(vec4(0.0,1.0,1.0,1.0)); 
