6db1d81426696b03433ef4d800765bf29c098ece
koltepri <odinnagust@protonmail.com>
2023-09-16 22:40:34 +0000

    syntax villa: gleymdi að bæta við this. i kallið

--- verkefni1/frogger.js
+++ verkefni1/frogger.js
@@ -154,6 +154,7 @@ class Car{
     this.position = this.initialCoordinates();
   }
   initialCoordinates() {
+    // mjög úreltuur kóði en virkar samt (gerdi þennan á undan Player grid dæminu)
     var coor = [];
     var boxYSize = 2/(3+nrOfLanes);
     let padding = 0.03;
@@ -170,24 +171,24 @@ class Car{
 class Player {
   constructor() {
     this.position = [0,3];
-    this.currentVertices = triangleVerticesFromCoordinate(0,3,Direction.UP); 
+    this.currentVertices = this.triangleVerticesFromCoordinate(0,3,Direction.UP); 
   }
   move(direction) { 
   // starting with instantanous moves, even if it trivializes the game
   // eins og stendur ma bara ekki fara utaf... þarf ad baeta vid vesen logic
     if (direction = Direction.UP) {
       this.position = [this.position[0]+1,this.position[1]];
-      this.currentVertices = triangleVerticesFromCoordinate(
+      this.currentVertices = this.triangleVerticesFromCoordinate(
         this.position[0],this.position[1]);
     } 
     else if (direction = Direction.RIGHT) {
       this.position = [this.position[0],this.position[1]+1];
-      this.currentVertices = triangleVerticesFromCoordinate(
+      this.currentVertices = this.triangleVerticesFromCoordinate(
         this.position[0],this.position[1]);
     } 
     else if (direction = Direction.LEFT) {
       this.position = [this.position[0]-1,this.position[1]];
-      this.currentVertices = triangleVerticesFromCoordinate(
+      this.currentVertices = this.triangleVerticesFromCoordinate(
         this.position[0],this.position[1]);
     } 
     else if (direction = Direction.DOWN) {
