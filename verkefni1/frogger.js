0b5eb0974532dab30778957fd7905c6503172ea9
koltepri <odinnagust@protonmail.com>
2023-09-17 14:02:05 +0000

    quickfix

--- verkefni1/frogger.js
+++ verkefni1/frogger.js
@@ -300,12 +300,12 @@ function coordinatesToGrid(boxCoordinates,lane,orientation) {
   var xCoor = boxCoordinates.map((value) => value[0]);
   for(let i = 0; i < row.length; i++) {
     if (orientation < 0) {
-      if (xCoor[0] < row[i][1] && xCoor[0] > row[i][1]) {
+      if (xCoor[0] < row[1] && xCoor[0] > row[1]) {
         return [lane,i];
       }
     }
     else {
-      if (xCoor[1] > row[i][0] && xCoor[1] < row[i][1]) {
+      if (xCoor[1] > row[0] && xCoor[1] < row[1]) {
         return [lane,i];
       }
     }
