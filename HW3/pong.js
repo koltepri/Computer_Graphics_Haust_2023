5bcab782013f86eeb49c14db2abac0d51e3841bc
koltepri <odinnagust@protonmail.com>
2023-09-19 12:16:36 +0000

    pong added, testing phase

---
+++ HW3/pong.js
@@ -0,0 +1,132 @@
+// Sameining
+var canvas;
+var gl;
+
+// Núverandi staðsetning miðju ferningsins
+var box = vec2( 0.0, 0.0 );
+
+// Stefna (og hraði) fernings
+var dX;
+var dY;
+
+// Svæðið er frá -maxX til maxX og -maxY til maxY
+var maxX = 1.0;
+var maxY = 1.0;
+
+// Hálf breidd/hæð ferningsins
+var boxRad = 0.05;
+
+// Ferningurinn er upphaflega í miðjunni
+var vertices = new Float32Array([-0.05, -0.05, 0.05, -0.05, 0.05, 0.05, -0.05, 0.05]);
+
+// ---
+var verticesPad;
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+    
+    // Gefa ferningnum slembistefnu í upphafi
+    dX = Math.random()*0.1-0.05;
+    dY = Math.random()*0.1-0.05;
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    locBox = gl.getUniformLocation( program, "boxPos" );
+
+    // Meðhöndlun örvalykla
+    window.addEventListener("keydown", function(e){
+        switch( e.keyCode ) {
+            case 38:	// upp ör
+                dX *= 1.1;
+                dY *= 1.1;
+                break;
+            case 40:	// niður ör
+                dX /= 1.1;
+                dY /= 1.1;
+                break;
+        }
+    } );
+
+    var verticesPad = [
+        vec2( -0.1, -0.9 ),
+        vec2( -0.1, -0.86 ),
+        vec2(  0.1, -0.86 ),
+        vec2(  0.1, -0.9 ) 
+    ];
+    
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(verticesPad), gl.DYNAMIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    // Event listener for keyboard
+    window.addEventListener("keydown", function(e){
+        switch( e.keyCode ) {
+            case 37:	// vinstri ör
+                xmove = -0.04;
+                break;
+            case 39:	// hægri ör
+                xmove = 0.04;
+                break;
+            default:
+                xmove = 0.0;
+        }
+        for(i=0; i<4; i++) {
+            verticesPad[i][0] += xmove;
+        }
+
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(verticesPad));
+    } );
+    render();
+}
+function render() {
+  gl.clear( gl.COLOR_BUFFER_BIT );
+
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(verticesPad));
+  gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 ); // error?
+    
+    // Láta ferninginn skoppa af veggjunum
+    if (Math.abs(box[0] + dX) > maxX - boxRad) dX = -dX;
+    if (Math.abs(box[1] + dY) > maxY - boxRad) dY = -dY;
+
+    // Uppfæra staðsetningu
+    box[0] += dX;
+    box[1] += dY;
+    
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    //
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
+    gl.uniform2fv( locBox, flatten(box) );
+
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+
+    window.requestAnimFrame(render);
+}
