ec78b095a70cd6a63252800e6b1eea2fcc6bd18a
koltepri <odinnagust@protonmail.com>
2023-09-19 14:30:49 +0000

    vesen ad teikna bada hluti

--- HW3/pong.js
+++ HW3/pong.js
@@ -107,26 +107,26 @@ window.onload = function init() {
 }
 function render() {
   gl.clear( gl.COLOR_BUFFER_BIT );
-
+  
+  // Drawing the pad
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(verticesPad));
   gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 ); // error?
     
-    // Láta ferninginn skoppa af veggjunum
-    if (Math.abs(box[0] + dX) > maxX - boxRad) dX = -dX;
-    if (Math.abs(box[1] + dY) > maxY - boxRad) dY = -dY;
+  // Láta ferninginn skoppa af veggjunum
+  if (Math.abs(box[0] + dX) > maxX - boxRad) dX = -dX;
+  if (Math.abs(box[1] + dY) > maxY - boxRad) dY = -dY;
 
-    // Uppfæra staðsetningu
-    box[0] += dX;
-    box[1] += dY;
+  // Uppfæra staðsetningu
+  box[0] += dX;
+  box[1] += dY;
     
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    //
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
-    gl.uniform2fv( locBox, flatten(box) );
+  // Drawing the ball 
+  bufferId = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.uniform2fv( locBox, flatten(box) );
+  gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
 
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
-
-    window.requestAnimFrame(render);
+  window.requestAnimFrame(render);
 }
